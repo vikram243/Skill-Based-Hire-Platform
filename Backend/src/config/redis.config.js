c517b7766c5f57bbae70b11a8327d67b61d739dd
Aashish <ashishkabirpanthi@gmail.com>
2026-01-15 12:33:13 +0530

    .

--- Backend/src/config/redis.config.js
+++ Backend/src/config/redis.config.js
@@ -1,4 +1,5 @@
 import { createClient } from 'redis';
+import config from './config.js';
 
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 const redis = createClient({ url: REDIS_URL });
