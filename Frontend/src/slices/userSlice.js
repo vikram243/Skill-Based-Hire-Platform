a2b40f7d9f7a5c169c8cfb9d887d2d3cf4e315f6
VIKRAM MISHRA <vikrammishra1042@gmail.com>
2026-01-16 21:01:00 +0530

    User data saved on Redux

--- Frontend/src/slices/userSlice.js
+++ Frontend/src/slices/userSlice.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import { createSlice } from "@reduxjs/toolkit";
 
-const userSlice = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const initialState = {
+  isAuthenticated: false,
+  loading: true, 
+  user: null,
+};
 
-export default userSlice
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.isAuthenticated = true;
+      state.user = action.payload;
+      state.loading = false;
+    },
+
+    logoutUser(state) {
+      state.isAuthenticated = false;
+      state.user = null;
+      state.loading = false;
+    },
+
+    setLoading(state, action) {
+      state.loading = action.payload;
+    },
+  },
+});
+
+export const { setUser, logoutUser, setLoading } = userSlice.actions;
+export default userSlice.reducer;
